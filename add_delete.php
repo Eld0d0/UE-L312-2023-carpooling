64ef108f1f51142a2f5c2ca6fed8a6641f4c3656
Damien Cuda <99328663+DamienCuda@users.noreply.github.com>
2023-11-27 15:51:19 +0100

    Branch [Annonce Convoit 1] : add_delete.php creation

---
+++ add_delete.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Controllers\AddsController;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$controller = new AddsController();
+echo $controller->deleteAdd();
+?>
+
+<p>Supression d'une annonce</p>
+<form method="post" action="add_delete.php" name ="addDeleteForm">
+    <label for="id">Numéro de l'annonce <em>Id</em> :</label>
+    <input type="text" name="id">
+    <br />
+    <input type="submit" value="Supprimer une annonce">
+</form>
