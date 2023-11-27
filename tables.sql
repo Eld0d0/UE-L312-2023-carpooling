2c5f1b39b66d82f7fbe61eab6b79c998542f7ef4
Eld0d0 <nils.gill.arion@gmail.com>
2023-11-27 12:32:05 +0100

    Update DataBase (SQL)

--- tables.sql
+++ tables.sql
@@ -11,3 +11,18 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (1, 'Vincent', 'Godé', 'hello@vincentgo.fr', '1990-11-08'),
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
 (3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
+
+CREATE TABLE `adverts` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `iddriver` varchar(255) NOT NULL,
+  `idcar` varchar(255) NOT NULL,
+  `citystart` varchar(255) NOT NULL,
+  `cityend` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `adverts` (`id`, `iddriver`, `idcar`, `citystart`, `cityend`, `date`) VALUES
+(1, '1', '2', 'Paris', 'Bordeaux', '2023-11-02'),
+(2, '3', '3', 'Marseille', 'Toulouse' '2023-12-05'),
+(3, '2', '1', 'Lyon', 'Strasbourg', '2023-12-11');
