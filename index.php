0eeeab74954299212988438e55b6574d878ff5a0
Nils <nils.gill.arion@gmail.com>
2023-11-29 00:00:12 +0100

    Mise à jour de la page index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
+<link rel="stylesheet" href="css/style.css" type="text/css" />
 <?php
+
 $dossier_pages = __DIR__;
 
 $contenu_dossier = scandir($dossier_pages);
@@ -7,18 +9,25 @@ $pages = array_filter($contenu_dossier, function($fichier) {
     return !is_dir($fichier) && pathinfo($fichier, PATHINFO_EXTENSION) == 'php'; 
 });
 
-if (!empty($pages)) {
-    echo '<ul>';
-    foreach ($pages as $page) {
-        $nom_fichier = pathinfo($page, PATHINFO_FILENAME);
-        $titre = str_replace('_', ' ', $nom_fichier);
-        echo '<li><a href="' . $page . '">' . ucfirst($titre) . '</a></li>'; 
-    }
-    echo '</ul>';
-} else {
-    echo 'Aucune page trouvée.';
+foreach ($pages as $page) {
+    $nom_fichier = pathinfo($page, PATHINFO_FILENAME);
+    $titre = str_replace('_', ' ', $nom_fichier);
+    $mot_debut = strtok($titre, " ");
+    $tableau[$mot_debut][] = [
+        'titre' => $titre,
+        'lien' => $page
+    ];
 }
-?>
-<style>
-    
-</style>
+
+echo '<h1>Menu de navigation</h1>';
+echo '<div id="menu">';
+foreach ($tableau as $mot => $liens) {
+    echo '<table>';
+    echo '<thead><tr><th colspan="100%">' . ucfirst($mot) . '</th></tr></thead>';
+    foreach ($liens as $lien) {
+        echo '<tr><td><a href="' . $lien['lien'] . '">' . ucfirst($lien['titre']) . '</a></td></tr>';
+    }
+    echo '</table>';
+}
+echo '</div>';
+
