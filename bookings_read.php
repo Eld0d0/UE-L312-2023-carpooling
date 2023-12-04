1144ba7b49dadde161c42a53e495bbad41c8a970
Eld0d0 <nils.gill.arion@gmail.com>
2023-12-04 01:00:13 +0100

    Update visuel Booking read

--- bookings_read.php
+++ bookings_read.php
@@ -5,9 +5,22 @@ use App\Controllers\BookingsController;
 require __DIR__ . '/vendor/autoload.php';
 
 $controller = new BookingsController();
-echo '<main>';
-echo $controller->getBookings();
-echo '</main>';
+
+echo '<main>
+        <table>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Annonce</th>
+                <th>Passager</th>
+            </tr>
+            </thead>
+            <tbody>';
+                echo $controller->getBookings();
+            echo '
+            </tbody>
+        </table>
+    </main>';
 
 ?>
 <link rel="stylesheet" href="css/form.css" type="text/css" />
